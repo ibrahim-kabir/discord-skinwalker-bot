a3bd487081d8a0939248b61796d703d5ddcdd0bf
HsKocTets <117961703+HsKocTets@users.noreply.github.com>
2024-02-16 01:48:59 -0500

    issue #1: service.py attributes

--- service.py
+++ service.py
@@ -3,16 +3,23 @@ import os
 
 class RecordingService:
     def __init__(self):
+        self.is_recording = False
+        self.channel_id = None 
+        print("Recording is ready")
         # ...
 
     def start_recording(self, channel):
+        print("start recording")
         # ...
 
     def stop_recording(self):
+        print("stop recording")
         # ...
 
     def save_audio(self, audio_data, filename):
+        print("save audio")
         # ...
 
     def delete_old_recordings(self):
+        print("delete old recordings")
         # ...
