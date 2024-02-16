8b10f3fff758a1150c938b029d53625669580ffd
HsKocTets <117961703+HsKocTets@users.noreply.github.com>
2024-02-16 17:52:17 -0500

    issue #1: commands implementation

--- bot.py
+++ bot.py
@@ -8,12 +8,40 @@ import random
 from discord import FFmpegPCMAudio
 
 class SkinWalker(commands.Bot):
-    def __init__(self, sentences_path,command_prefix, *args, **kwargs):
-        super().__init__(command_prefix,*args, **kwargs)
-        self.recording_service = RecordingService()
-        self.load_settings()
+    def __init__(self, recording_service: RecordingService, sentences_path: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.recording_service = recording_service
         self.sentences_path = sentences_path
+        self.load_settings()
+        self.add_commands()
 
+    def add_commands(self):
+        @self.command(name="status", pass_context=True)
+        async def status(ctx):
+            print(ctx)
+            await ctx.channel.send("Up and running!")
+        
+        @self.command(name="set_appearance_frequency", pass_context=True)
+        async def set_appearance_frequency(ctx, frequency: int):
+            self.appearance_frequency = frequency
+            await ctx.send(f'Appearance frequency has been set to {frequency}')
+
+        @self.command(name="set_min_user", pass_context=True)
+        async def set_min_user(ctx, user: int):
+            self.min_users = user
+            await ctx.send(f'Minimum users have been set to {user}')
+
+        @self.command(name="get_settings", pass_context=True)
+        async def get_settings(ctx):
+            await ctx.channel.send(f'Appearance frequency: {self.appearance_frequency}, Minimum users: {self.min_users}')
+
+        @self.command(name="leave", help="Makes the bot leave the voice channel.", pass_context=True)
+        async def leave(ctx):
+            if ctx.voice_client is not None:
+                await ctx.voice_client.disconnect()
+            else:
+                await ctx.send("I'm not connected to a voice channel.")   
+        
     def load_settings(self):
         with open('settings.json', 'r') as file:
             settings = json.load(file)
@@ -44,39 +72,21 @@ class SkinWalker(commands.Bot):
         connection = await channel.connect()
         print(os.path.isdir(self.sentences_path + mp3_file), self.sentences_path + mp3_file)
         connection.play(FFmpegPCMAudio(self.sentences_path + mp3_file))
-        await channel.disconnect()
-
-    @commands.command(name="set_appearance_frequency")
-    async def set_appearance_frequency(self, ctx, frequency: int):
-        self.appearance_frequency = frequency
-        await ctx.send(f'Appearance frequency has been set to {frequency}')
-
-    @commands.command(name="set_min_user")
-    async def set_min_user(self, ctx, user: int):
-        self.min_users = user
-        await ctx.send(f'Minimum users have been set to {user}')
-
-    @commands.command(name="get_settings")
-    async def get_settings(self, ctx):
-        await ctx.send(f'Appearance frequency: {self.appearance_frequency}, Minimum users: {self.min_users}')
-
-    @commands.command(help="Makes the bot leave the voice channel.")
-    async def leave(self, ctx):
-        print("leave")
-        try:
-            await ctx.voice_client.disconnect()
-        except Exception as e:
-            print(e)
+        await self.disconnect()
 
 if __name__ == "__main__":
     load_dotenv()
-    token = os.environ.get("DISCORD_TOKEN")
+    skinwalker_token = os.environ.get("SKINWALKER_TOKEN")
+    recording_service_token = os.environ.get("RECORDING_SERVICE_TOKEN")
     sentences_path = os.environ.get("SENTENCES_DIRECTORY_PATH")
 
     intents = discord.Intents.default()
     intents.message_content = True
+    
+    recording_service = RecordingService()
+    recording_service.run(recording_service_token)
 
-    client = SkinWalker(sentences_path, command_prefix='/', intents=intents)
-    client.run(token)
+    skinwalker_client = SkinWalker(recording_service, sentences_path, command_prefix='/', intents=intents, self_bot=False)
+    skinwalker_client.run(skinwalker_token)
 
 
