99cc97de886cc85b6a59c7904797f933aca13fff
HsKocTets <117961703+HsKocTets@users.noreply.github.com>
2024-02-16 00:39:34 -0500

    issue #1: join the voice chat

--- bot.py
+++ bot.py
@@ -1,15 +1,27 @@
 import discord
 from discord.ext import commands
-from service import RecordingService
 from dotenv import load_dotenv
 import os
 
-class SkinWalker(discord.Client):
+class SkinWalker(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+    async def on_voice_state_update(self, member, before, after):
+        try:
+            print(f"User {member} triggered a voice event on the {after.channel} channel")
+            user_count = len(after.channel.members)
+            if user_count >= 2:
+                try:
+                    await after.channel.connect()  # Join the voice channel
+                    print(f"Joined voice channel: {after.channel.name}")
+                except discord.errors.ClientException as e:
+                    print(f"Error joining voice channel: {e}")
+        except AttributeError:
+            print("No channel found, user left the chat")
 
 if __name__ == "__main__":
     load_dotenv()
@@ -18,4 +30,4 @@ if __name__ == "__main__":
     intents.message_content = True
 
     client = SkinWalker(intents=intents)
-    client.run()
+    client.run(token)
