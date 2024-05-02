0bedad711ae12096b80e3da0ce470b394c5f3522
Ibrahim Kabir <117961703+HsKocTets@users.noreply.github.com>
2024-05-01 22:22:02 -0400

    issue #3: debug

--- app/skinwalker_bot.py
+++ app/skinwalker_bot.py
@@ -16,7 +16,7 @@ class SkinWalker(interactions.Client):
         self.recording_service_id = int(recording_service_id)
         self.recording_path = recording_path
         self.load_settings()
-        self.channels_recording = []
+        self.channels_recording = set()
         
     @slash_command(name="set_appearance_frequency", description="Set the frequency with which the bot makes appearances in voice channels.")
     async def set_appearance_frequency(self, ctx, frequency: int):
@@ -30,7 +30,7 @@ class SkinWalker(interactions.Client):
             # Check if the recording service is in the channel        
             for member in event.after.channel.members: 
                 if member.id == self.recording_service_id:
-                    self.channels_recording.append(event.after.channel)
+                    self.channels_recording.add(event.after.channel)
                     self.join_channel_randomly.start()
                     return
             if event.after.channel in self.channels_recording:
@@ -50,21 +50,23 @@ class SkinWalker(interactions.Client):
         with open('settings.json', 'w') as file:
             json.dump(settings, file)
 
-    @Task.create(IntervalTrigger(minutes=1))
+    @Task.create(IntervalTrigger(seconds=10))
     async def join_channel_randomly(self):
         if random.random() < (100 / self.appearance_frequency):
             for channel in self.channels_recording:
                 await self.play_random_mp3(channel)
-        raise ValueError("1m has passed")
-
+                raise ValueError(self.channels_recording)
+        raise ValueError("10s has passed")
 
     async def play_random_mp3(self, channel):
         try:
+            if not os.path.exists(self.recording_path):
+                os.makedirs(self.recording_path)
             mp3_files = [f for f in os.listdir(self.recording_path) if f.endswith('.mp3')]
             mp3_file = random.choice(mp3_files)
             mp3_path = self.recording_path + mp3_file
             audio = AudioVolume(mp3_path)
-            audio_duration = len(AudioSegment.from_mp3(mp3_path)) / 1000  # duration in seconds
+            audio_duration = len(AudioSegment.from_mp3(mp3_path)) / 1000 + 1 # duration in seconds
             
             voice_client = await channel.connect()
             await voice_client.play(audio)
